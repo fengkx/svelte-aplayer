a28b30ff79730fe1f2489e1ba47f598dce32ac15
fengkx <liangkx8237@gmail.com>
2023-02-19 10:24:43 +0800

    chore: update deps

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,31 @@
 import * as path from "path";
 import { defineConfig } from "vite";
-import svelte from "@sveltejs/vite-plugin-svelte";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
 import image from "@rollup/plugin-image";
-import gitRevSync from "git-rev-sync";
 import pkg from "./package.json";
+import git from "async-git";
+import dts from "vite-plugin-dts";
 
 const customElement =
   process.env.CUSTOM_ELEMENT !== "0" && process.env.CUSTOM_ELEMENT !== "false";
 // https://vitejs.dev/config/
 export default defineConfig({
   define: {
-    GIT_HASH: `'${gitRevSync.short()}'`,
+    GIT_HASH: `'${await git.short}'`,
     SVELTE_APLAYER_VERSION: `'${pkg.version}'`,
   },
   build: {
-    outDir: path.resolve(__dirname, "dist", customElement ? 'wc' : 'svelte'),
+    outDir: path.resolve(__dirname, "dist", customElement ? "wc" : "svelte"),
     lib: {
       entry: path.resolve(__dirname, "src/main.ts"),
       name: "Aplayer",
     },
   },
-  plugins: [svelte(), image({})],
+  plugins: [
+    svelte(),
+    image({}),
+    // dts({
+    //   outputDir: ["dist", "types"],
+    // }),
+  ],
 });
